1ffb5dc46fcd74ff5a7b98b2d07f25ba5d5bce30
Kemal Crisannaufal <107296586+kemalcrisannaufal@users.noreply.github.com>
2022-12-30 08:11:32 +0700

    Add files via upload

---
+++ HitungIPK.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+using namespace std;
+
+const int nMax = 10;
+
+typedef string arrMK[nMax];
+typedef int arrSKS[nMax];
+typedef string arrIdx[nMax];
+
+struct dataMK {
+    arrMK tabMK;
+    arrSKS tabSKS;
+    arrIdx tabIdx;
+    int N;
+};
+
+float getPoint(string index) {
+    if (index == "A") {
+        return 4;
+    } else if (index == "AB") {
+        return 3.5;
+    } else if (index == "B") {
+        return 3; 
+    } else if (index == "BC") {
+        return 2.5;
+    } else if (index == "C") {
+        return 2;
+    } else if (index == "D") {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+float Poin(int SKS, float point) {
+    return SKS*point;
+}
+
+float totalPoin(dataMK dataCourse) {
+    float t_poin = 0;
+
+    for (int i = 0; i < dataCourse.N; i++) {
+        t_poin = t_poin + Poin(dataCourse.tabSKS[i], getPoint(dataCourse.tabIdx[i]));
+    }
+    return t_poin;
+}
+
+
+void inputData(dataMK &dataCourse) {
+    for (int i = 0; i < dataCourse.N; i++) {
+        cout << "[" << i+1 << "]" << endl;
+        cout << "Mata Kuliah : ";
+        cin >> dataCourse.tabMK[i];
+        cout << "SKS         : ";
+        cin >> dataCourse.tabSKS[i];
+        cout << "Index Nilai : ";
+        cin >> dataCourse.tabIdx[i];
+        cout << endl;
+    }
+}
+
+int totalSKS(dataMK dataCourse) {
+    int t_sks = 0;
+    for (int i = 0; i < dataCourse.N; i++) {
+        t_sks = t_sks + dataCourse.tabSKS[i];
+    }
+    return t_sks;    
+}
+
+float finalIPK(dataMK dataCourse){
+    return totalPoin(dataCourse) / totalSKS(dataCourse);
+}
+
+int main() {
+    dataMK MK;
+    
+    cout << "Jumlah Mata Kuliah = ";
+    cin >> MK.N;
+
+    inputData(MK);
+    cout << "IPK = " << finalIPK(MK) << endl;
+
+}
+
+/**
+ 8
+KWU
+2
+AB
+MATVEK
+3
+A
+TEPEL
+3
+A
+WGTIK
+2
+A
+STRUKTURDATA
+4
+A
+AKA
+2
+A
+OAP
+3
+A
+SBD
+3
+A
+*/
